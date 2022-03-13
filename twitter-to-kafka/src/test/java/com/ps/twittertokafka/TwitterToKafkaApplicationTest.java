6dacd114d60565c1c5e62c3c8a4a39a1af75c35d
Prashant Sharma <prashant.sharma1thoughtworks.com>
2022-03-14 00:32:39 +0530

    Added skeleton for twitter-to-kafka microservice

---
+++ twitter-to-kafka/src/test/java/com/ps/twittertokafka/TwitterToKafkaApplicationTest.java
@@ -0,0 +1,14 @@
+package com.ps.twittertokafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterToKafkaApplicationTest {
+
+    @Test
+    public void contextLoad(){
+
+    }
+
+}
