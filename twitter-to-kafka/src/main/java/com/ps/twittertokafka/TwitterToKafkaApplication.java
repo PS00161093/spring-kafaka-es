5eaa3a5e1dd1765c606da934d3b9d28eaab86bdf
Prashant Sharma <prashant.sharma1thoughtworks.com>
2022-03-14 01:39:08 +0530

    Added Tweets Runner & reading live tweets

--- twitter-to-kafka/src/main/java/com/ps/twittertokafka/TwitterToKafkaApplication.java
+++ twitter-to-kafka/src/main/java/com/ps/twittertokafka/TwitterToKafkaApplication.java
@@ -1,11 +1,13 @@
 package com.ps.twittertokafka;
 
 import com.ps.twittertokafka.config.Config;
+import com.ps.twittertokafka.runner.StreamRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import twitter4j.TwitterException;
 
 import java.util.Arrays;
 
@@ -14,9 +16,12 @@ public class TwitterToKafkaApplication implements CommandLineRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(TwitterToKafkaApplication.class);
 
+    private final StreamRunner streamRunner;
+
     private final Config config;
 
-    public TwitterToKafkaApplication(Config config) {
+    public TwitterToKafkaApplication(StreamRunner streamRunner, Config config) {
+        this.streamRunner = streamRunner;
         this.config = config;
     }
 
@@ -25,9 +30,11 @@ public class TwitterToKafkaApplication implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) {
-        System.out.println("Twitter to Kafka app starts!");
+    public void run(String... args) throws TwitterException {
+        logger.info("Twitter to Kafka app starts!");
         logger.info(config.getWelcomeMessage());
         logger.info(Arrays.toString(config.getTwitterKeywords().toArray(new String[]{})));
+        streamRunner.start();
     }
+
 }
---
+++ twitter-to-kafka/src/main/java/com/ps/twittertokafka/runner/impl/TwitterStreamRunnerImpl.java
@@ -0,0 +1,54 @@
+package com.ps.twittertokafka.runner.impl;
+
+import com.ps.twittertokafka.config.Config;
+import com.ps.twittertokafka.listener.TweetsListener;
+import com.ps.twittertokafka.runner.StreamRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import twitter4j.FilterQuery;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+
+import javax.annotation.PreDestroy;
+import java.util.Arrays;
+import java.util.Objects;
+
+@Component
+public class TwitterStreamRunnerImpl implements StreamRunner {
+
+    private final static Logger logger = LoggerFactory.getLogger(TwitterStreamRunnerImpl.class);
+
+    private final TweetsListener tweetsListener;
+
+    private TwitterStream twitterStream;
+
+    private final Config config;
+
+    public TwitterStreamRunnerImpl(TweetsListener tweetsListener, Config config) {
+        this.tweetsListener = tweetsListener;
+        this.config = config;
+    }
+
+    @Override
+    public void start() {
+        twitterStream = new TwitterStreamFactory().getInstance();
+        twitterStream.addListener(tweetsListener);
+        addFilter();
+    }
+
+    private void addFilter() {
+        String[] keywords = config.getTwitterKeywords().toArray(new String[0]);
+        FilterQuery filterQuery = new FilterQuery(keywords);
+        twitterStream.filter(filterQuery);
+        logger.info("Started filtering tweets for keywords {}", Arrays.toString(keywords));
+    }
+
+    @PreDestroy
+    public void shutdown() {
+        if (Objects.nonNull(twitterStream)) {
+            logger.info("Closing twitter stream.");
+            twitterStream.shutdown();
+        }
+    }
+}
