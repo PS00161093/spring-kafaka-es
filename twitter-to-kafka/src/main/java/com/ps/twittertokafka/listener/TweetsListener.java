c6fe672edd021badf0bb80dc7d47f64e0260b4d4
Prashant Sharma <prashant.sharma1thoughtworks.com>
2022-03-14 01:19:13 +0530

    Added Tweets listener

---
+++ twitter-to-kafka/src/main/java/com/ps/twittertokafka/listener/TweetsListener.java
@@ -0,0 +1,18 @@
+package com.ps.twittertokafka.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import twitter4j.Status;
+import twitter4j.StatusAdapter;
+
+@Component
+public class TweetsListener extends StatusAdapter {
+
+    private final static Logger logger = LoggerFactory.getLogger(TweetsListener.class);
+
+    @Override
+    public void onStatus(Status status) {
+        logger.info("Tweet: {}", status.getText());
+    }
+}
