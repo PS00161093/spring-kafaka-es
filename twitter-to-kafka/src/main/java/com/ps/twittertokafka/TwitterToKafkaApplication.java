28e7cf7e6e57c49121f90eacc12bff614b8cf3c5
Prashant Sharma <prashant.sharma1thoughtworks.com>
2022-03-14 00:52:32 +0530

    Added config for reading keywords from Twitter feeds

--- twitter-to-kafka/src/main/java/com/ps/twittertokafka/TwitterToKafkaApplication.java
+++ twitter-to-kafka/src/main/java/com/ps/twittertokafka/TwitterToKafkaApplication.java
@@ -1,11 +1,32 @@
 package com.ps.twittertokafka;
 
+import com.ps.twittertokafka.config.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class TwitterToKafkaApplication {
+public class TwitterToKafkaApplication implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(TwitterToKafkaApplication.class);
+
+    private final Config config;
+
+    public TwitterToKafkaApplication(Config config) {
+        this.config = config;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(TwitterToKafkaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println("Twitter to Kafka app starts!");
+        logger.info(Arrays.toString(config.getTwitterKeywords().toArray(new String[]{})));
     }
 }
---
+++ twitter-to-kafka/src/main/java/com/ps/twittertokafka/config/Config.java
@@ -0,0 +1,14 @@
+package com.ps.twittertokafka.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@ConfigurationProperties(prefix = "twitter-to-kafka-service")
+@Data
+public class Config {
+    private List<String> twitterKeywords;
+}
