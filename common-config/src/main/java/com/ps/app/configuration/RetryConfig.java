e4fc6f0ef5d8c3b5a3f620dba64e63d3fbd22751
Prashant Sharma <prashant.sharma1thoughtworks.com>
2022-03-14 23:54:45 +0530

    Configured kafka-admin & Retry config

---
+++ common-config/src/main/java/com/ps/app/configuration/RetryConfig.java
@@ -0,0 +1,37 @@
+package com.ps.app.configuration;
+
+import com.ps.app.configiguration.RetryConfigData;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.backoff.ExponentialBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+
+@Configuration
+@ComponentScan(basePackages = "com.ps.app")
+public class RetryConfig {
+
+    private final RetryConfigData retryConfigData;
+
+    public RetryConfig(RetryConfigData retryConfigData) {
+        this.retryConfigData = retryConfigData;
+    }
+
+    @Bean
+    public RetryTemplate retryTemplate() {
+        ExponentialBackOffPolicy backOffPolicy = new ExponentialBackOffPolicy();
+        backOffPolicy.setInitialInterval(retryConfigData.getInitialInterval());
+        backOffPolicy.setMaxInterval(retryConfigData.getMaxInterval());
+        backOffPolicy.setMultiplier(retryConfigData.getMultiplier());
+
+        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
+        simpleRetryPolicy.setMaxAttempts(retryConfigData.getMaxAttempts());
+
+        RetryTemplate retryTemplate = new RetryTemplate();
+        retryTemplate.setBackOffPolicy(backOffPolicy);
+        retryTemplate.setRetryPolicy(simpleRetryPolicy);
+
+        return retryTemplate;
+    }
+}
