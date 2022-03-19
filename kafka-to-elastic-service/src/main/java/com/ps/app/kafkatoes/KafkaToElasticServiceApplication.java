82caabcc2083cfab390251d42120a4d4769a966c
Prashant Sharma <prashant.sharma1thoughtworks.com>
2022-03-19 15:13:06 +0530

    Added kafka-to-es module

---
+++ kafka-to-elastic-service/src/main/java/com/ps/app/kafkatoes/KafkaToElasticServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ps.app.kafkatoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.ps.app")
+public class KafkaToElasticServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaToElasticServiceApplication.class, args);
+    }
+}
